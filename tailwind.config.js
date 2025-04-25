fbeab52a40f2366d7bd63d2a087cbfaf36bf8edb
Ярослав Сигурко <aroslavsigurko@MacBook-Air-Aroslav.local>
2025-04-26 00:46:14 +0300

    chore: подготовка конфигов и обновление WebApp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{ts,tsx}'
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'anonymous-pro': ['"Anonymous Pro"', 'monospace']
-      }
-    }
+        'anonymous-pro': ['"Anonymous Pro"', 'monospace'],
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
